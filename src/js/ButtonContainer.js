041d1782b54db520d36950d0040bbe609a57cd02
pablomartan <pablomartan@kolabnow.com>
2023-04-10 13:04:39 +0200

    feat: convert ButtonContainer component to function

--- src/js/ButtonContainer.js
+++ src/js/ButtonContainer.js
@@ -1,90 +1,85 @@
 import React from 'react';
 import { Button } from './Button';
 
-export class Buttons extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+export const ButtonContainer = () => {
+  const btnNames = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'equals',
+    'add',
+    'subtract',
+    'multiply',
+    'divide',
+    'decimal',
+    'clear'
+  ];
 
-  render() {
-    const btnNames = [
-      'zero',
-      'one',
-      'two',
-      'three',
-      'four',
-      'five',
-      'six',
-      'seven',
-      'eight',
-      'nine',
-      'equals',
-      'add',
-      'subtract',
-      'multiply',
-      'divide',
-      'decimal',
-      'clear'
-    ];
+  const nameToNumber = s => {
+    let tag;
+    switch(s) {
+      case 'zero':
+        tag = '0';
+        break;
+      case 'one':
+        tag =  '1';
+        break;
+      case 'two':
+        tag =  '2';
+        break;
+      case 'three':
+        tag =  '3';
+        break;
+      case 'four':
+        tag =  '4';
+        break;
+      case 'five':
+        tag =  '5';
+        break;
+      case 'six':
+        tag = '6';
+        break;
+      case 'seven':
+        tag =  '7';
+        break;
+      case 'eight':
+        tag =  '8';
+        break;
+      case 'nine':
+        tag =  '9';
+        break;
+      default:
+        tag =  s;
+    }
+  };
 
-    const nameToNumber = s => {
-      switch(s) {
-        case 'zero':
-          return '0';
-          break;
-        case 'one':
-          return '1';
-          break;
-        case 'two':
-          return '2';
-          break;
-        case 'three':
-          return '3';
-          break;
-        case 'four':
-          return '4';
-          break;
-        case 'five':
-          return '5';
-          break;
-        case 'six':
-          return'6';
-          break;
-        case 'seven':
-          return '7';
-          break;
-        case 'eight':
-          return '8';
-          break;
-        case 'nine':
-          return '9';
-          break;
-        default:
-          return s;
-      }
-    };
+  const value = b => {
+    tag =  b === 'equals' ? '='
+    : b === 'add' ? '+'
+    : b === 'subtract' ? '-'
+    : b === 'multiply' ? 'X'
+    : b === 'divide' ? '/'
+    : b === 'decimal' ? '.'
+    : b === 'clear' ? 'AC'
+    : nameToNumber(b)
+  };
 
-    const value = b => {
-      return b === 'equals' ? '='
-      : b === 'add' ? '+'
-      : b === 'subtract' ? '-'
-      : b === 'multiply' ? 'X'
-      : b === 'divide' ? '/'
-      : b === 'decimal' ? '.'
-      : b === 'clear' ? 'AC'
-      : nameToNumber(b)
-    };
-
-    return(
-      <>
-        <div id='button-container' className=''>
-          {btnNames.map((b, i) => {
-            return(
-              <Button key={i} className='calc-btn' id={b} value={value(b)} />
-            )
-          })}
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <div id='button-container'>
+        {btnNames.map((b, i) => {
+          return (
+            <Button key={i} className='calc-btn' id={b} value={value(b)} />
+          )
+        })}
+      </div>
+    </>
+  )
 }
