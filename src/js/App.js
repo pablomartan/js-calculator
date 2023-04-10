90deead528cdcb6df9fed4773fb62d509e2abe02
pablomartan <pablomartan@kolabnow.com>
2023-04-10 12:46:06 +0200

    feat: convert react App component to function

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import { ButtonContainer } from './ButtonContainer.js';
+import { Display } from './Display.js';
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return(
-      <div>
-        <div id='app'></div>
+export function App() {
+  return(
+      <div id='app' className='col-10 col-xs-8 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-0 m-auto'>
+        <Display />
+        <ButtonContainer />
       </div>
-  }
+  )
 }
