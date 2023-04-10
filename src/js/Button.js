f16183b529f5e051d04cae14c45ee62832af226a
pablomartan <pablomartan@kolabnow.com>
2023-04-10 12:43:15 +0200

    feat: add Button react component

---
+++ src/js/Button.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { buttonPressed } from './features/calculatorSlice';
+
+export function Button(props) {
+  const dispatch = useDispatch();
+
+  return(
+    <button className='calc-btn' id={props.id}
+    onClick={e => dispatch(buttonPressed(e.target.value))} value={props.value}>
+    {props.value}
+    </button>
+  )
+}
