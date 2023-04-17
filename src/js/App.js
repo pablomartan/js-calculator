999842a25a4e1120ed410d7f3a8ac7d30f732df9
pablomartan <pablomartan@kolabnow.com>
2023-04-17 22:49:31 +0200

    feat: add some styling improvements

--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,7 @@ import { Display } from './Display.js';
 
 export function App() {
   return(
-      <div id='app' className='col-10 col-xs-8 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-0 m-auto'>
+      <div id='app' className='col-10 col-xs-8 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-0 m-auto shadow'>
         <Display />
         <ButtonContainer />
       </div>
