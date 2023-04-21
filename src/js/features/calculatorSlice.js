6352829f9e13d52c60077528dd549e784d9edd16
pablomartan <pablomartan@kolabnow.com>
2023-04-21 17:31:47 +0200

    feat: change switch for object lookup in calculator reducer
    After reading about the benefits of object lookups vs switch
    statements, decided to change it in my code. The reducer is cleaner now,
    and the functionality is moved to a calculatorSliceHelpers file, with
    all the helper functions and constants (different commit)

--- src/js/features/calculatorSlice.js
+++ src/js/features/calculatorSlice.js
@@ -1,14 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
+import * as Helpers from './calculatorSliceHelpers';
 
-const operators = ['-', '+', '/', 'X', '*'];
-
-const removeSigns = str => {
-  console.log(str);
-  if (!operators.includes(str.slice(-1))) {
-    return str;
-  } else {
-    return removeSigns(str.slice(0, str.length - 1));
-  }
+const buttonDict = {
+  '0': Helpers.handleNum,
+  '1': Helpers.handleNum,
+  '2': Helpers.handleNum,
+  '3': Helpers.handleNum,
+  '4': Helpers.handleNum,
+  '5': Helpers.handleNum,
+  '6': Helpers.handleNum,
+  '7': Helpers.handleNum,
+  '8': Helpers.handleNum,
+  '9': Helpers.handleNum,
+  '-': Helpers.handleOperation,
+  '+': Helpers.handleOperation,
+  '/': Helpers.handleOperation,
+  'X': Helpers.handleOperation,
+  '=': Helpers.handleEqual,
+  '.': Helpers.handleDot,
+  'AC': Helpers.handleClear, 
 };
 
 export const calculatorSlice = createSlice({
@@ -19,63 +29,8 @@ export const calculatorSlice = createSlice({
   },
   reducers: {
     buttonPressed: (state, action) => {
-      const input = state.input;
-      const output = state.output;
-      const val = action.payload;
-
-      switch(val) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-          if (operators.includes(input)) {
-            state.input = val;
-            state.output += val;
-          } else if (input == '0' || output.includes('=')) {
-            state.input = state.output = val;
-          } else {
-            state.input += val;
-            state.output += val;
-          }
-          break;
-        case '-':
-        case '+':
-        case '/':
-        case 'X':
-          // makes sure the sign appended to output can be parsed by eval()
-          const sign = val == 'X' ? '*' : val;
-          state.input = val;
-          
-          if (state.output.includes('=')) {
-            state.output = input + sign;
-          } else if (operators.includes(state.output.slice(-1)) && val != '-') {
-            state.output = removeSigns(state.output).concat(sign);
-          } else {
-            state.output += sign;
-          }
-          break;
-        case 'AC':
-          state.input = '0';
-          state.output = '';
-          break;
-        case '.':
-          if (!input.includes('.')) {
-            state.input += '.';
-            state.output += '.';
-          }
-          break;
-        case '=':
-          if (!state.output.includes('=') && state.output != '') {
-            state.input = eval(output);
-            state.output += String('=' + state.input);
-          }
-      }
+      const handler = buttonDict[action.payload];
+      handler(state, action.payload);
     }
   }
 });
